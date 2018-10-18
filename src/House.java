29928b1ad51c18ce7be722d5b5ae9154139c20f2
MichaelCMD <michael.j.crowleymcdevitt@students.ittralee.ie>
2018-10-18 15:48:03 +0100

    idk

--- src/House.java
+++ src/House.java
@@ -3,19 +3,13 @@ public class House {
     String address, owner, type;
     double price;
 
-  /* public House(String address, String type, double price, String owner) {
-       this("No Address Specified","No Type Specified", 0.0,"No type Specified");
-   }
-
-    public void setAddress(String address) {
-
-    } */
-
     public House() {
+
         price = 0.0;
         address  = "No Address";
         type = "No Type";
         owner = "No Owner";
+
     }
 
     public Book(String address, String owner, String type, double price) {
@@ -24,6 +18,7 @@ public class House {
         this.owner = owner;
         this.type = type;
         this.price = price;
+
     }
 
     public void setPrice(double price) {
